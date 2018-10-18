f69542790df8b2db4c66c14c69739c9674ad5f7f
uzoltan <zoltan.umlauf@gmail.com>
2018-10-18 13:32:26 +0200

    pom xml updates. also tried adding JPMS, but used libraries are not yet ready for it

---
+++ asset-tracking-library/src/main/java/eu/arrowhead/assettrackinglibrary/PlaceHolder.java
@@ -0,0 +1,5 @@
+package eu.arrowhead.assettrackinglibrary;
+
+public class PlaceHolder {
+
+}
--- asset-tracking-library/src/test/java/eu/arrowhead/assettrackinglibrary/AssetTrackingLibraryApplicationTests.java
+++ asset-tracking-library/src/test/java/eu/arrowhead/assettrackinglibrary/AssetTrackingLibraryApplicationTests.java
@@ -2,6 +2,7 @@ package eu.arrowhead.assettrackinglibrary;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +12,11 @@ public class AssetTrackingLibraryApplicationTests {
 
   @Test
   public void contextLoads() {
+  }
+
+  @SpringBootApplication
+  static class TestConfiguration {
+
   }
 
 }
